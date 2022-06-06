a0461d4d167e9617ee9747d69ad0c056a455ca4f
Tyrone North <tnorthjr@gmail.com>
2022-06-06 15:11:19 -0500

    Updated styling

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+
+Donezo.Repo.insert!(%Donezo.Accounts.User{
+  email: "test@test.com", password: "passwordisLong1!", hashed_password: Pbkdf2.hash_pwd_salt("passwordisLong1!")}
+)
+
+Donezo.Repo.insert!(%Donezo.Notes.Task{
+  title: "First Tasks!", items: ["Create a new task. Click the 'NEW TASK' button near the top of the screen.", "Delete this item."], user_id: 1})
+
+Donezo.Repo.insert!(%Donezo.Notes.Task{
+  title: "Second Tasks!", items: ["Edit this task. Change the title and add or remove items", "You can add as many items as you like."], user_id: 1})
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\donezo" eval Donezo.Release.migrate
